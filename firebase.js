7bfa3e061000ba5e9a5fba69aedf3dbb3fc71b10
WBorges07 <wbns3121@gmail.com>
2026-01-20 14:14:41 -0300

    Format firebase.js and ensure proper imports

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// translate=no
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
