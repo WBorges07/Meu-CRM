229b77925d35a86c6c19211f4de230d001cf30fb
WBorges07 <wbns3121@gmail.com>
2026-01-20 13:51:36 -0300

    Update Firebase configuration with new project details

--- firebase.js
+++ firebase.js
@@ -1,16 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID", // ESTE É O CAMPO QUE ESTÁ FALTANDO
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAv7cufs_iYhyeQRhq_XVtOIJQYEl2FBvo",
+  authDomain: "customers-3bc70.firebaseapp.com",
+  projectId: "customers-3bc70",
+  storageBucket: "customers-3bc70.firebasestorage.app",
+  messagingSenderId: "591091207016",
+  appId: "1:591091207016:web:44234d9033eb650e0d64dd",
+  measurementId: "G-L1VQD35MB3"
 };
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
